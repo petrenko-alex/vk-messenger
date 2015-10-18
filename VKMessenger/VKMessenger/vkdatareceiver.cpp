261c49bc5748745fe62d0a2b8bcb0f26a22b754c
Александр Петренко <gafk555@gmail.com>
2015-10-18 10:45:04 +0300

    В класс VKDataReceiver добавлен функционал загрузки фото пользователя.

--- VKMessenger/VKMessenger/vkdatareceiver.cpp
+++ VKMessenger/VKMessenger/vkdatareceiver.cpp
@@ -16,23 +16,36 @@ VKDataReceiver::~VKDataReceiver()
 
 void VKDataReceiver::sendRequest(const QString &methodName,const QList<QPair<QString,QString> > &parametres)
 {
-	this->methodName = methodName;
-
-	/* Создаем URL запроса на основе переданного имени метода API */
-	QString stringUrl = "https://api.vk.com/method/" + methodName;
-	QUrl apiRequestUrl(stringUrl);
-
-	/* Добавляем параметры запроса в URL */
-	QUrlQuery query;
-	for (auto i : parametres)
+	if (!methodName.isEmpty() && !parametres.isEmpty())
 	{
-		query.addQueryItem(i.first, i.second);
-	}
-	apiRequestUrl.setQuery(query);
+		this->methodName = methodName;
 
-	/* Посылаем запрос */
-	QNetworkRequest request(apiRequestUrl);
-	networkManager->get(request);
+		/* Создаем URL запроса на основе переданного имени метода API */
+		QString stringUrl = "https://api.vk.com/method/" + methodName;
+		QUrl apiRequestUrl(stringUrl);
+
+		/* Добавляем параметры запроса в URL */
+		QUrlQuery query;
+		for (auto i : parametres)
+		{
+			query.addQueryItem(i.first, i.second);
+		}
+		apiRequestUrl.setQuery(query);
+
+		/* Посылаем запрос */
+		QNetworkRequest request(apiRequestUrl);
+		networkManager->get(request);
+	}
+}
+
+void VKDataReceiver::loadPhoto(const QUrl &photoUrl)
+{
+	if (!photoUrl.isEmpty())
+	{
+		this->photoUrl = photoUrl;
+		QNetworkRequest request(photoUrl);
+		networkManager->get(request);
+	}
 }
 
 void VKDataReceiver::getReply(QNetworkReply *reply)
@@ -42,6 +55,10 @@ void VKDataReceiver::getReply(QNetworkReply *reply)
 	if (reply->url().path() == ("/method/" + methodName))
 	{
 		emit dataReceived(data);
+	}
+	else if (reply->url().toDisplayString() == photoUrl.toDisplayString())
+	{
+		emit photoReceived(data);
 	}
 	else
 	{
--- VKMessenger/VKMessenger/vkdatareceiver.h
+++ VKMessenger/VKMessenger/vkdatareceiver.h
@@ -18,16 +18,19 @@ class VKDataReceiver : public QObject
 
 signals:
 	void dataReceived(const QByteArray &data);
+	void photoReceived(const QByteArray &data);
 
 public:
 	VKDataReceiver();
 	~VKDataReceiver();
 
 	void sendRequest(const QString &methodName, const QList<QPair<QString, QString> > &parametres);
+	void loadPhoto(const QUrl &photoUrl);
 
 private:
 	QNetworkAccessManager *networkManager;
 	QString methodName;
+	QUrl photoUrl;
 
 private slots:
 	void getReply(QNetworkReply *reply);
