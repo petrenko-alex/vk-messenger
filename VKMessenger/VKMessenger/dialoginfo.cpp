33f2b3f355d96c625154694dcf5292d8041861c0
Александр Петренко <gafk555@gmail.com>
2015-11-01 15:54:38 +0300

    Изменен конструктор класса DilogInfo и добавлена перегрузка оператора =.

--- VKMessenger/VKMessenger/dialoginfo.cpp
+++ VKMessenger/VKMessenger/dialoginfo.cpp
@@ -1,17 +1,30 @@
 #include "dialoginfo.h"
 
-DialogInfo::DialogInfo(QWidget *parent)
-	: QWidget(parent)
+DialogInfo::DialogInfo(/*QWidget *parent, */unsigned int userId, unsigned int messageId, QString &title, QString &lastMessage, QDateTime &lastMessageDateTime, bool out) /*: QWidget(parent)*/
 {
 	ui.setupUi(this);
-}
-
-DialogInfo::DialogInfo(const QString &userId, const QString &lastMessage)
-{
-
+	this->userId = userId;
+	this->messageId = messageId;
+	this->title = title;
+	this->lastMessage = lastMessage;
+	this->lastMessageDateTime = lastMessageDateTime;
+	this->out = out;
 }
 
 DialogInfo::~DialogInfo()
 {
 
+}
+
+DialogInfo & DialogInfo::operator=(const DialogInfo &other)
+{
+	this->userId = other.userId;
+	this->messageId = other.messageId;
+	this->title = other.title;
+	this->lastMessage = other.lastMessage;
+	this->username = other.username;
+	this->lastMessageDateTime = other.lastMessageDateTime;
+	this->out = other.out;
+
+	return *this;
 }
--- VKMessenger/VKMessenger/dialoginfo.h
+++ VKMessenger/VKMessenger/dialoginfo.h
@@ -8,21 +8,28 @@
 #include "ui_dialoginfo.h"
 #include "vkdatareceiver.h"
 
+
 class DialogInfo : public QWidget
 {
 	Q_OBJECT
 
 public:
-	DialogInfo(QWidget *parent = 0);
-	DialogInfo(const QString &userId, const QString &lastMessage);
+	DialogInfo( /*QWidget *parent, */
+			    unsigned int userId,unsigned int messageId,
+				QString &title,QString &lastMessage,
+				QDateTime &lastMessageDateTime, bool out);
 	~DialogInfo();
+	DialogInfo & operator=(const DialogInfo &other);
 
 private:
 	Ui::DialogInfo ui;
 	unsigned int userId;
+	unsigned int messageId;
+	QString title;
 	QString username;
 	QString lastMessage;
 	QDateTime lastMessageDateTime;
+	bool out;
 	/* Добавить фото */
 };
 
