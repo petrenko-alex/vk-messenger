184a7ddf2fe0cabb0f0ac821b01babba3fe3cb95
Александр Петренко <gafk555@gmail.com>
2015-11-13 22:14:32 +0300

    Добавлены комментарии в файл dialoginfo.h.

--- VKMessenger/VKMessenger/dialoginfo.h
+++ VKMessenger/VKMessenger/dialoginfo.h
@@ -23,7 +23,6 @@
 
 enum DialogType { PERSONAL, CHAT };
 
-
 class DialogInfo : public QWidget
 {
 	Q_OBJECT
@@ -33,48 +32,91 @@ public:
 				QString &title,QString &lastMessage,
 				QDateTime &lastMessageDateTime, bool out);
 	~DialogInfo();
+
 	DialogInfo & operator=(const DialogInfo &other);
 
+	/* Загрузить данные о собеседнике */
 	void loadOpponentInfo();
+
+	/* Загрузить фото собеседника */
 	void loadOpponentPhoto(QString photoUrl);
+
+	/* Загрузить сообщения диалога */
 	void loadMessages();
+
+	/* Добавить новое сообщение к диалогу (входящее) */
 	void addMessage(MessageType type,const QString &fromId, const QString &text);
+
+	/* Отправить сообщение собеседнику */
 	void sendMessage(QString &msg);
 
+	/* Получить id собеседника */
 	QString getId() const;
+
+	/* Получить имя собеседника */
 	QString getUserName() const;
+
+	/* Проверить, инициализирован ли диалог - были ли загружены его сообщения */
 	bool isInitialized() const;
+
+	/* Установить последнее сообщение диалога в виджет */
 	void setLastMessage(const QString &text);
+
+	/* Окрасить рамку виджета в красный цвет - признак наличия нового сообщения в диалоге */
 	void paintFrameRed();
+
+	/* Получить виджет сообщений */
 	QWidget * getMessagesWidget() const;
 
 signals:
+	/* Сигнал - сообщения диалога загружены */
 	void messagesLoaded(QWidget *scrollWidget, QString &username);
+
+	/* Сигнал - сообщение отправлено */
 	void messageWasSent(QWidget *dialog);
 
 private:
 	/* Фильтр событий */
 	bool eventFilter(QObject *obj, QEvent *event);
+
+	/* Установить данные в виджет диалога */
 	void setDataToWidgets();
+
+	/* Распарсить сообщения диалога */
 	void parseMessages(const QByteArray &messages);
+
+	/* Окрасить рамку виджета в белый цвет - нормальное состояние */
 	void paintFrameWhite();
+
+	/* Обработать клик по виджету диалога */
 	void mouseReleaseEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
+
+	/* Загрузить фото собеседника по id (для диалога типа CHAT) */
 	QByteArray loadPhotoByUserId(unsigned int userId);
 
 	Ui::DialogInfo ui;
-	unsigned int id;
-	QString name;
-	QString lastMessage;
-	QDateTime lastMessageDateTime;
-	QByteArray photo;
-	bool out;
-	bool clicked;
-	bool initialized;
-	bool clickBlock;
 
+	/* Данные диалога */
+	bool out;
+	QString name;
+	unsigned int id;
+	QByteArray photo;
+	QString lastMessage;
 	DialogType dialogType;
+	QDateTime lastMessageDateTime;
+
+	/* Флаги, управляющие поведением виджета диалога */
+	bool clicked;
+	bool clickBlock;
+	bool initialized;
+
+	/* Модуль получения данных с ВК */
 	VKDataReceiver *dataReceiver;
+
+	/* Список сообщений диалога */
 	QList<AbstractMessage *> userMessages;
+
+	/* Виджет сообщение диалога */
 	QWidget *messagesScrollWidget;
 };
 
